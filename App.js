5e2aa261bb23e0f569ff59f372b1d046d4da8717
Minha Kwon <namisoft@naver.com>
2023-02-11 00:45:58 +0900

    theme 적용(styled-components)

--- App.js
+++ App.js
@@ -6,16 +6,22 @@ import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import Root from "./navigation/Root";
+import { ThemeProvider } from "styled-components/native";
+import { useColorScheme } from "react-native";
+import { darkTheme, lightTheme } from "./styled";
 
 export default function App() {
   const [assets] = useAssets([require("./myImage.jpg")]);
   const [fonts] = Font.useFonts(Ionicons.font);
+  const isDark = useColorScheme() === "dark";
   if (!assets || !fonts) {
     return <AppLoading />;
   }
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
