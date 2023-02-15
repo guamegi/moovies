541dd315e5712a18417d0a07491cf5a438291888
Minha Kwon <namisoft@naver.com>
2023-02-15 14:42:40 +0900

    poster 작업

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,37 +1,66 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
-import Swiper from "react-native-web-swiper";
-import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
+import Swiper from "react-native-swiper";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
 import { makeImgPath } from "../utils";
 import { BlurView } from "expo-blur";
 
 const API_KEY = "0c64ca8a40141078588fcf6759d30cde";
 
 const Container = styled.ScrollView``;
-
 const View = styled.View`
   flex: 1;
 `;
-
 const Loader = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
-
 const BgImg = styled.Image``;
-
-const Title = styled.Text``;
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+`;
+const Title = styled.Text`
+  font-size: 16px;
+  font-weight: 600;
+  color: white;
+`;
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+const Column = styled.View`
+  width: 50%;
+  margin-left: 15px;
+`;
+const Overview = styled.Text`
+  color: rgba(255, 255, 255, 0.6);
+  margin-top: 10px;
+`;
+const Vote = styled(Overview)`
+  font-size: 12px;
+`;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
+  const isDark = useColorScheme() === "dark";
+
   useEffect(() => {
     getNowPlaying();
   }, []);
+
   const getNowPlaying = async () => {
     const { results } = await (
       await fetch(
@@ -41,6 +70,7 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
     setNowPlaying(results);
     setLoading(false);
   };
+
   return loading ? (
     <Loader>
       <ActivityIndicator />
@@ -49,8 +79,9 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
     <Container>
       <Swiper
         loop
-        timeout={3}
-        controlsEnabled={false}
+        autoplay={true}
+        autoplayTimeout={3}
+        showsPagination={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
         {nowPlaying.map((movie) => (
@@ -59,8 +90,21 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
               source={{ uri: makeImgPath(movie.backdrop_path) }}
               style={StyleSheet.absoluteFill}
             />
-            <BlurView intensity={100} style={StyleSheet.absoluteFill}>
-              <Title>{movie.title}</Title>
+            <BlurView
+              tint={isDark ? "dark" : "light"}
+              intensity={90}
+              style={StyleSheet.absoluteFill}
+            >
+              <Wrapper>
+                <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
+                <Column>
+                  <Title>{movie.title}</Title>
+                  {movie.vote_average > 0 ? (
+                    <Vote>⭐️{movie.vote_average}/10</Vote>
+                  ) : null}
+                  <Overview>{movie.overview.slice(0, 90)}...</Overview>
+                </Column>
+              </Wrapper>
             </BlurView>
           </View>
         ))}
