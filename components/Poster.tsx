9c8f5eb2406929d9d3d69db3ae70b18570fbf5b2
Minha Kwon <namisoft@naver.com>
2023-02-17 19:15:02 +0900

    coming soon

--- components/Poster.tsx
+++ components/Poster.tsx
@@ -5,6 +5,8 @@ import { makeImgPath } from "../utils";
 const Image = styled.Image`
   width: 100px;
   height: 160px;
+  border-radius: 5px;
+  background-color: rgba(255, 255, 255, 0.5);
 `;
 
 interface PosterProps {
