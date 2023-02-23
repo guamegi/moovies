275285f09ee538845186c866d5094c0e73685ebb
Minha Kwon <namisoft@naver.com>
2023-02-23 22:52:35 +0900

    detail: title 전달

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, useColorScheme } from "react-native";
 import Detail from "../screens/Detail";
+import { BLACK_COLOR } from "../colors";
 
 const NativeStack = createNativeStackNavigator();
 const Stack = () => {
+  const isDark = useColorScheme() === "dark";
   return (
-    <NativeStack.Navigator screenOptions={{ headerBackTitleVisible: false }}>
+    <NativeStack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerStyle: { backgroundColor: isDark ? BLACK_COLOR : "white" },
+        headerTitleStyle: { color: isDark ? "white" : BLACK_COLOR },
+      }}
+    >
       <NativeStack.Screen name="Detail" component={Detail} />
     </NativeStack.Navigator>
   );
